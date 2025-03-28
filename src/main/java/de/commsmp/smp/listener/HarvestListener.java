b295d67aa1d4b30ad72fdd24662e791bd2a455cf
ezTxmMC <eztxmmc@hotmail.com>
2025-03-28 14:55:06 +0100

    added drop amount on harvest

--- src/main/java/de/commsmp/smp/listener/HarvestListener.java
+++ src/main/java/de/commsmp/smp/listener/HarvestListener.java
@@ -9,6 +9,8 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Random;
+
 public class HarvestListener implements Listener {
 
     @EventHandler
@@ -16,38 +18,34 @@ public class HarvestListener implements Listener {
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
             return;
         }
-
         Block block = event.getClickedBlock();
         if (block == null) {
             return;
         }
-
-        if (block.getBlockData() instanceof Ageable) {
-            Ageable ageable = (Ageable) block.getBlockData();
-
-            if (ageable.getAge() == ageable.getMaximumAge()) {
-                event.setCancelled(true);
-                Material dropMaterial = getDropMaterial(block.getType());
-                if (dropMaterial != null) {
-                    block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(dropMaterial));
-                }
-                ageable.setAge(0);
-                block.setBlockData(ageable);
+        if (block.getBlockData() instanceof Ageable ageable) {
+            if (ageable.getAge() != ageable.getMaximumAge()) {
+                return;
             }
+            event.setCancelled(true);
+            Material dropMaterial = getDropMaterial(block.getType());
+            if (dropMaterial != null) {
+                block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(
+                        dropMaterial,
+                        new Random().nextInt(3)
+                ));
+            }
+            ageable.setAge(0);
+            block.setBlockData(ageable);
         }
     }
 
     private Material getDropMaterial(Material blockType) {
-        switch (blockType) {
-            case CARROTS:
-                return Material.CARROT;
-            case POTATOES:
-                return Material.POTATO;
-            case BEETROOTS:
-                return Material.BEETROOT;
-            default:
-                return blockType;
-        }
+        return switch (blockType) {
+            case CARROTS -> Material.CARROT;
+            case POTATOES -> Material.POTATO;
+            case BEETROOTS -> Material.BEETROOT;
+            case WHEAT_SEEDS -> Material.WHEAT;
+            default -> blockType;
+        };
     }
-
 }
